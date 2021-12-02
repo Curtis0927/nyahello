677bd2a101631855884f7ce6b052b0ad2487300c
Curtis0927 <91111311+Curtis0927@users.noreply.github.com>
2021-12-01 19:38:59 -0800

    idk

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
@@ -11,21 +10,5 @@ btn.addEventListener("click", function(){
 
 })
 function getRandomNumber(){
-    return Math.floor(Math.random() * colors.length;
+    return Math.floor(Math.random() * colors.length);
 }
-=======
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
-
-btn.addEventListener("click", function(){
- const randomNumber = getRandomNumber();
-
- document.body.style.backgroundColor = colors[randomNumber];
- color,this.textContent = colors[randomNumber];
-
-})
-function getRandomNumber(){
-    return Math.floor(Math.random() * colors.length;
-}
->>>>>>> a9884edd8797f9933a5ffad2ee0b0bb397a9b491
