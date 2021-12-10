1316e92bcf932afd7a96c7873bc315f479489f37
Curtis0927 <91111311+Curtis0927@users.noreply.github.com>
2021-12-10 09:19:39 -0800

    Update app.js

--- navbar/setup/app.js
+++ navbar/setup/app.js
@@ -4,9 +4,9 @@
 // remove - remove class
 // toggle - toggles class
 
-const navToggle = document.querySelector(.nav-toggle);
-const links = document.querySelector(.links);
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 
-navToggle.addEventListener('click',function(){
+navToggle.addEventListener('click', function(){
 links.classList.toggle("show-links");
 });
