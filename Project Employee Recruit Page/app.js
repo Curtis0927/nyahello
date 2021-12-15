096d885239050aaa8c22a6f5a59daa91d4951945
Curtis0927 <91111311+Curtis0927@users.noreply.github.com>
2021-12-15 11:15:46 -0800

    yes.

--- Project Employee Recruit Page/app.js
+++ Project Employee Recruit Page/app.js
@@ -1,31 +1,47 @@
+//like dislike//
 let count = 0;
 
-const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn")
+const value = document.querySelector("#likeDislike");
+const likes = document.querySelectorAll(".like")
 
-console.log(btns)
-
-btns.forEach(function (btn) {
-    btn.addEventListener("click", function (e) {
+likes.forEach(function (like) {
+    like.addEventListener("click", function (e) {
       const styles = e.currentTarget.classList;
-      if (styles.contains("decrease")) {
-        count--;
-      } else if (styles.contains("increase")) {
-        count++;
-      } else {
-        count = 0;
-      }
-  
-      if (count > 0) {
-        value.style.color = "green";
-      }
-      if (count < 0) {
-        value.style.color = "red";
-      }
-      if (count === 0) {
-        value.style.color = "#222";
-      }
-      value.textContent = count;
-    });
-  });
-  
+
+          if (styles.contains("down")) {
+            count--;
+          } else if (styles.contains("up")) {
+            count++;
+          } else {
+            count = 0;
+          }
+      
+          if (count > 0) {
+            //change color to white later//
+            value.style.color = "blue";
+          }
+          if (count < 0) {
+            value.style.color = "red";
+          }
+          if (count === 0) {
+            value.style.color = "black";
+          }
+          value.textContent = count;
+        });
+      });
+      //color switch//
+      const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    let hexColor = "#";
+    for(let i = 0;i<6;i++){
+        hexColor += hex[getRandomNumber()];
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor= hexColor;
+})
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+}
