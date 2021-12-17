389191f77318c185529347a9d28aa693c5f9b57f
Curtis0927 <91111311+Curtis0927@users.noreply.github.com>
2021-12-17 00:33:40 -0800

    reeeeeeeeeeeeeeeeeeeeeee

--- Project Employee Recruit Page/app.js
+++ Project Employee Recruit Page/app.js
@@ -1,47 +1,50 @@
 //like dislike//
 let count = 0;
+let number2 = 2;
 
 const value = document.querySelector("#likeDislike");
-const likes = document.querySelectorAll(".like")
+const likes = document.querySelectorAll(".like");
 
 likes.forEach(function (like) {
-    like.addEventListener("click", function (e) {
-      const styles = e.currentTarget.classList;
+  like.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
 
-          if (styles.contains("down")) {
-            count--;
-          } else if (styles.contains("up")) {
-            count++;
-          } else {
-            count = 0;
-          }
-      
-          if (count > 0) {
-            //change color to white later//
-            value.style.color = "blue";
-          }
-          if (count < 0) {
-            value.style.color = "red";
-          }
-          if (count === 0) {
-            value.style.color = "black";
-          }
-          value.textContent = count;
-        });
-      });
-      //color switch//
-      const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
-
-btn.addEventListener('click', function(){
-    let hexColor = "#";
-    for(let i = 0;i<6;i++){
-        hexColor += hex[getRandomNumber()];
+    if (styles.contains("down")) {
+      count--;
+    } else if (styles.contains("up")) {
+      count++;
+      count++;
+    } else {
+      count = 0;
     }
-    color.textContent = hexColor;
-    document.body.style.backgroundColor= hexColor;
-})
-function getRandomNumber(){
-    return Math.floor(Math.random() * hex.length);
+
+    if (count > 0) {
+      //change color to white later//
+      value.style.color = "white";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style;
+      e.color = "black";
+    }
+    value.textContent = count;
+  });
+});
+//color switch//
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btnSwitch");
+const color = document.querySelectorAll("color");
+
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
 }
